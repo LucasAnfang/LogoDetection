05a0ab3e3667863ec5b6ea1a6e5a90130cd087b2
luke anfang <lanfang@usc.edu>
2017-09-10 16:23:30 -0700

    progress on storage connector

---
+++ storage_controller/test.py
@@ -0,0 +1,14 @@
+from storage_manager import LogoStorageConnector
+lsc = LogoStorageConnector()
+
+lsc.upload_brand_training_input_data("Patagonia", "Hat")
+lsc.upload_brand_training_input_data("Patagonia", "fun")
+
+lsc.upload_brand_operational_input_data("Patagonia", "Shirt with logo")
+lsc.upload_brand_operational_input_data("Patagonia", "Star Wars")
+
+lsc.upload_brand_operational_output_data("Patagonia", "Star Wars Rules")
+lsc.upload_brand_operational_output_data("Patagonia", "Star Trek Drules")
+
+lsc.pretty_print_storage_structure();
+lsc.download_brand_training_input_data("Patagonia")
