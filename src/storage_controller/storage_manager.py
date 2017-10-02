5744002e314e1fda97faf50a94783cf71b19bc74
luke anfang <lanfang@usc.edu>
2017-10-01 19:44:32 -0700

    optimized upload by using multithreaded upload of blocks

--- src/storage_controller/storage_manager.py
+++ src/storage_controller/storage_manager.py
@@ -11,6 +11,10 @@ import json
 from log_entries import *
 from instagram_post_entity import InstagramPostEntities
 from io import BytesIO
+from io import BytesIO
+import zlib
+import threading
+import base64
 from PIL import Image
 
 class LogoStorageConnector:
@@ -32,28 +36,14 @@ class LogoStorageConnector:
 
 	""" Public Interfaces """
 	""" Upload """
-	# def upload_brand_training_input_data(self, brand, data, isProcessed):
-	# 	container_name = self._input_container()
-	# 	#path = '{}/{}'.format(brand, self.constants.TRAINING_DIRECTORY_NAME)
-	# 	path = self._create_path_to_bucket(brand, self.constants.TRAINING_DIRECTORY_NAME)
-	# 	full_blob_name = self._upload_data(container_name, path, data)
-	# 	self.log(full_blob_name, isProcessed)
-	# 	return  full_blob_name
-
-	# def upload_brand_operational_input_data(self, brand, data, isProcessed):
-	# 	container_name = self._input_container()
-	# 	path = '{}/{}'.format(brand, self.constants.OPERATIONAL_DIRECTORY_NAME)
-	# 	full_blob_name = self._upload_data(container_name, path, data)
-	# 	self.log(full_blob_name, isProcessed)
-	# 	return  full_blob_name
 
 	def upload_brand_training_input_IPE(self, brand, IPE, isProcessed):
-		return self.upload_IPE_to_bucket(self._input_container(), brand, self.constants.TRAINING_DIRECTORY_NAME, IPE, isProcessed)
+		return self.upload_IPE_to_bucket(self._input_container(), brand, self.constants.TRAINING_DIRECTORY_NAME, IPE, isProcessed, log = True)
 
 	def upload_brand_operational_input_IPE(self, brand, IPE, isProcessed):
-		return self.upload_IPE_to_bucket(self._input_container(), brand, self.constants.OPERATIONAL_DIRECTORY_NAME, IPE, isProcessed)
+		return self.upload_IPE_to_bucket(self._input_container(), brand, self.constants.OPERATIONAL_DIRECTORY_NAME, IPE, isProcessed, log = True)
 
-	def upload_IPE_to_bucket(self, container_name, brand, directory, IPE, isProcessed):
+	def upload_IPE_to_bucket(self, container_name, brand, directory, IPE, isProcessed, log = False):
 		bucket_path = self._create_path_to_bucket(brand, directory)
 		bucket_post_entities_full_path = self._get_bucket_post_entities_file(bucket_path)
 		bucket_images_base_path = self._get_bucket_image_directory(bucket_path)
@@ -65,13 +55,9 @@ class LogoStorageConnector:
 				element.pop('picture', None)
 				element['image_path'] = image_path
 		self._upload_text(container_name, bucket_post_entities_full_path, IPE.serialize())
-		self.log(bucket_path, isProcessed)
+		if(log == True):
+			self.log(bucket_path, isProcessed)
 		return  bucket_path
-
-	# def upload_brand_operational_output_data(self, brand, data):
-	# 	container = self._output_container()
-	# 	path = '{}/{}'.format(brand, self.constants.OPERATIONAL_DIRECTORY_NAME)
-	# 	return self._upload_data(container, path, data)
 
 	""" Download """
 	def download_brand_training_input_data(self, brand, processing_status_filter = None):
@@ -147,9 +133,16 @@ class LogoStorageConnector:
 	def _output_container(self):
 		return self.constants.OUTPUT_CONTAINER_NAME
 
+	def get_parent_directory(self, entity):
+		return entity.rsplit('/', 1)[0]
+			
+	def exists(self, container, full_blob_name = None):
+		return self.service.exists(container, full_blob_name)
+
 	def _upload_text(self, container_name, full_blob_name, data):
 		if not(self.exists(container_name)):
 			self._create_container(container_name)
+		print("uploading text to path", full_blob_name)
 		self.service.create_blob_from_text(container_name, full_blob_name, data)
 		return full_blob_name
 
@@ -157,12 +150,69 @@ class LogoStorageConnector:
 		if not(self.exists(container_name)):
 			self._create_container(container_name)
 		full_blob_name = '{}{}'.format(path, '.png')
+
 		with BytesIO() as output:
 			data.save(output, 'PNG')
 			bytes = output.getvalue()
+
 		print("uploading image to path", path)
-		self.service.create_blob_from_bytes(container_name, full_blob_name, bytes)
+		self._parallel_upload(container_name, full_blob_name, bytes)
 		return full_blob_name
+
+	def _parallel_upload(self, container_name, full_blob_name, data):
+		debug = True
+		threads = []
+		thread_count = 10
+		block_ids = []
+
+		current_index = 0
+		end_index = len(data) - 1
+
+		offset = end_index / thread_count
+
+		n = len(data) / 5
+		if (debug):
+			print("chunking data into even sections of length: ", n)
+		chunks = [data[i:i + n] for i in xrange(0, len(data), n)]
+
+		for chunk in chunks:
+			uid = self.generate_uid()#[str(uuid.uuid4())[:4]]
+			block_ids.append(BlobBlock(id=uid))
+			if (debug):
+				print("spawning thread with uid: ", uid)
+			t = threading.Thread(target=self._upload_block, args=(container_name,full_blob_name,chunk,uid,))
+			threads.append(t)
+		if (debug):	
+			print("starting all the threads...")
+		[t.start() for t in threads]
+		if (debug):	
+			print("all threads started...")
+		[t.join() for t in threads]
+		if (debug):	
+			print("all threads have completed execution")
+
+		block_list = self.service.get_block_list(container_name, full_blob_name, block_list_type=BlockListType.All)
+		uncommitted = len(block_list.uncommitted_blocks)
+		committed = len(block_list.committed_blocks)
+		if (debug):	
+			print("uncommitted: ", uncommitted, " committed: ", committed)
+
+		if (debug):	
+			print("committing blocks")
+		self.service.put_block_list(container_name, full_blob_name, block_ids)
+
+		block_list = self.service.get_block_list(container_name, full_blob_name, block_list_type=BlockListType.All)
+		uncommitted = len(block_list.uncommitted_blocks)
+		committed = len(block_list.committed_blocks)
+		if (debug):	
+			print("uncommitted: ", uncommitted, " committed: ", committed)
+
+	def _upload_block(self, container_name, full_blob_name, chunk, uid):
+		self.service.put_block(container_name, full_blob_name, chunk, uid)
+
+	def generate_uid(self):
+		r_uuid = base64.urlsafe_b64encode(uuid.uuid4().bytes)
+		return r_uuid.replace('=', '')
 
 	def _download_data(self, container_name, full_blob_name):
 		if not(self.exists(container_name)):
@@ -195,11 +245,11 @@ class LogoStorageConnector:
 		raw = log_entries.serialize()
 		self.service.create_blob_from_text(container_name, log_path, raw)
 
-	def update_log_entries(self, full_blob_names, isProcessed):
+	def update_log_entries(self, bucket_name, isProcessed):
 		directories = {}
 		container_name = self._input_container()
 		for full_blob_name in full_blob_names:
-			path = self.get_blobs_parent_directory(full_blob_name)
+			path = self.get_blobs_parent_directory(bucket_name)
 			log_path = path + '/log.txt'
 			if log_path in directories:
 				directories[log_path].append(full_blob_name)
@@ -216,9 +266,3 @@ class LogoStorageConnector:
 				log_entries.update(full_blob_name, isProcessed=isProcessed)
 			raw = log_entries.serialize()
 			self.service.create_blob_from_text(container_name, log_path, raw)
-		
-	def get_parent_directory(self, entity):
-		return entity.rsplit('/', 1)[0]
-			
-	def exists(self, container, full_blob_name = None):
-		return self.service.exists(container, full_blob_name)
