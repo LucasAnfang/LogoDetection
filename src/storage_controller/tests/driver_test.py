08cf341967b94c038d8a9284e05dc7f5cad2e18d
Luke Anfang <lanfang@usc.edu>
2017-10-22 22:51:05 -0700

    new conn string

--- src/storage_controller/tests/driver_test.py
+++ src/storage_controller/tests/driver_test.py
@@ -19,8 +19,8 @@ if(test_tables == True):
 
 # config = './nfs_test_config'
 # config_path = os.path.abspath(input_config_name)
-STORAGE_ACCOUNT_NAME = 'logodetectiontesting'
-STORAGE_ACCOUNT_KEY = 'HF0EwhCG2R8BBeKGV5qrloyz5Ua0kYQlSQI/vDWsTv3AjjK2nDJOD6Y8iLPjtF6nMnJr2zQZ0xhxkDF0biCArg=='
+STORAGE_ACCOUNT_NAME = 'logodetectionstorage'
+STORAGE_ACCOUNT_KEY = 'jPJyzct+8WD1lKU5M+ZwDflWUGRu+YBpH8n/3Z6qR7WD7uc3HV2U1rtiQKesLRq2tU3jtXIe26RklAYdKzoydA=='
 config = NFS_Controller_Config(STORAGE_ACCOUNT_NAME, STORAGE_ACCOUNT_KEY)
 if(test_new_driver == True):
 	driver = Driver(config)
